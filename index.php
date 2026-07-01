e7eb7c7b104392a1537245388c9a6193752be219
maftusylla <mamefatousylla003@gmail.com>
2026-07-01 00:22:52 +0000

    feat: ajout prodruit a une categorie

---
+++ index.php
@@ -0,0 +1,21 @@
+ $categorieExiste =  false;
+          $code = readline("veuillez saisir le code :");
+             foreach ($categories as $index => $categorie ) {
+               if (($categorie["code"]) === $code) {
+                    $categorieExiste = true;
+                    break;
+         }
+       } 
+
+       if ($categorieExiste) {
+        $produit =   [
+                    "nom" => readline("veuillez saisir le nom : "),
+                    "reference" => readline("veuillez saisir la reference : "),
+                    "prix" => (int)readline("veuillez saisir le prix : "),
+                    "quantite" => (int)readline("veuillez saisir la quantité : ")
+                  ] ;
+          $categories[$index]["produits"][] = $produit;
+       }else {
+          echo "Cette categorie n'existe pas ";
+       }
+       
